d17013f8edccf8594778d13868c5703d894a6c18
Emil Fredriksson <fredrikssonemil@hotmail.com>
2025-11-25 22:25:07 +0100

    fix: exist update script after 3 min if still no internet connection

--- custom.sh
+++ custom.sh
@@ -4,9 +4,16 @@
 # then update explodelon to latest version at startup if it can connect to github.
 # You have to restart recalbox for the new version to end up in the gamelist.xml.
 
+TIMEOUT=180
 while ! ping -c 1 -W 1 api.github.com; do
     echo "Waiting for api.github.com - network might not be connected"
     sleep 1
+
+    TIMEOUT=$((TIMEOUT-1))
+    if [ $TIMEOUT -eq 0 ]; then
+        echo "timed out waiting for network connection"
+        exit 1
+    fi
 done
 
 echo "OK, network is up!"
