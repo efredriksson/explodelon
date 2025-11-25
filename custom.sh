c01a5e8a09794bbcf87df857e620f8e76db84340
Emil Fredriksson <fredrikssonemil@hotmail.com>
2025-11-25 21:31:41 +0100

    feat: script for automatic updates to recalbox

---
+++ custom.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Place this script in /recalbox/share/system on your recal box system. It will
+# then update explodelon to latest version at startup if it can connect to github.
+# You have to restart recalbox for the new version to end up in the gamelist.xml.
+
+while ! ping -c 1 -W 1 api.github.com; do
+    echo "Waiting for api.github.com - network might not be connected"
+    sleep 1
+done
+
+echo "OK, network is up!"
+
+ROM_DIR=/recalbox/share/roms/lutro
+DOWNLOAD_LINK="$(curl -s https://api.github.com/repos/efredriksson/explodelon/releases/latest \
+| grep "browser_download_url.*lutro" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+)"
+GAME_NAME_AND_VERISON="$(echo ${DOWNLOAD_LINK} | xargs -- basename)"
+
+# Download game to ROM folder:
+mkdir -p ${ROM_DIR}
+wget -O ${ROM_DIR}/${GAME_NAME_AND_VERISON} ${DOWNLOAD_LINK}
