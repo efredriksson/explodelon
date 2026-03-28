f2ef235ac4c71a2c168d074541b604ec5ca4ee76
Emil Fredriksson <fredrikssonemil@hotmail.com>
2026-03-28 19:34:43 +0100

    test(formatter): add testing for formatter

---
+++ spec/formatter/rewriter_spec.lua
@@ -0,0 +1,77 @@
+local helpers = require("spec.formatter.helpers")
+
+describe("formatter", function()
+
+   describe("comma spacing", function()
+      it("adds a single space after commas where spacing is wrong", helpers.format([[
+         local x = f(1,  2,  3)
+         local y = f("a","b")
+      ]], [[
+         local x = f(1, 2, 3)
+         local y = f("a", "b")
+      ]]))
+
+      it("does not change a comma that already has exactly one space", helpers.check([[
+         local x = f(1, 2, 3)
+      ]]))
+
+      it("does not touch commas where the next token is on the next line", helpers.check([[
+         local x = f(1,
+            2)
+      ]]))
+   end)
+
+   describe("trailing spaces", function()
+      it("removes trailing whitespace from lines", helpers.format([[
+         local x = 1   
+         local y = 2
+         local z = x + y  
+      ]], [[
+         local x = 1
+         local y = 2
+         local z = x + y
+      ]]))
+
+      it("leaves already-clean source unchanged", helpers.check([[
+         local x = 1
+         local y = 2
+      ]]))
+   end)
+
+   describe("fmt: off / fmt: on", function()
+      it("skips formatting between directives and resumes after", helpers.format([[
+         local function example()
+            -- fmt: off
+            local a = {1,  2,  3}
+            local b = {4,  5,  6}
+            -- fmt: on
+            local c = {7,  8,  9}
+         end
+      ]], [[
+         local function example()
+            -- fmt: off
+            local a = {1,  2,  3}
+            local b = {4,  5,  6}
+            -- fmt: on
+            local c = {7, 8, 9}
+         end
+      ]]))
+
+      it("fmt: off without fmt: on is scoped to the enclosing block", helpers.format([[
+         local function inner()
+            -- fmt: off
+            local a = {1,  2,  3}
+         end
+
+         local b = {4,  5,  6}
+      ]], [[
+         local function inner()
+            -- fmt: off
+            local a = {1,  2,  3}
+         end
+
+         local b = {4, 5, 6}
+      ]]))
+   end)
+
+end)
